ba703605145217ab02ed0dc40e3fa186b00fd842
callumaoconnor <93537697+callumaoconnor@users.noreply.github.com>
2021-11-04 16:56:32 -0400

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const config = {
   title: "",
   description:
     "Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.",
-  sideBarInfo: ["Title", "img src=Thumbnail", "Location", "Summary", "Date Published"],
+  sideBarInfo: ["Title", "projectthumbnail", "Location", "Summary", "Date Published"],
   popupInfo: ["Title"],
   filters: [
     {
@@ -69,3 +69,8 @@ const config = {
     },
   ],
 };
+
+  var img = document.createElement("img"); 
+  img.src = "${row["thumbnail"]}"; 
+  var src = document.getElementById("projectthumbnail"); 
+  src.appendChild(img); 

ccaf69f564a6349e6958936ff4d1eaa7642be064
callumaoconnor <93537697+callumaoconnor@users.noreply.github.com>
2021-11-04 17:12:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,7 @@ const config = {
   title: "",
   description:
     "Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.",
-  sideBarInfo: ["Title", "Location", "Summary", "Date Published"],
-  sideBarInfo += `"<img scr="${row["image"]}"/>"`,
+  sideBarInfo: [`"<img scr="${row["image"]}"/>"`, "Title", "Location", "Summary", "Date Published"],
   popupInfo: ["Title"],
   filters: [
     {

e5683f99f5635530163b19a477e1fdb9dbb56b9e
callumaoconnor <93537697+callumaoconnor@users.noreply.github.com>
2021-11-08 18:36:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ function buildLocationList(locationData) {
     for (let i = 1; i < columnHeaders.length; i++) {
       const div = document.createElement("div");
       div.innerText += prop[columnHeaders[i]];
-      div.className;
+      div.className = "item-detail-co";
       details.appendChild(div);
     }
 

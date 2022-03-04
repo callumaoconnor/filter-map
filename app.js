c39cbc7d40a2b2653518e3f9e7faebd51fb6c137
callumaoconnor <93537697+callumaoconnor@users.noreply.github.com>
2022-03-04 15:11:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ function buildLocationList(locationData) {
     const learnMore = listing.appendChild(document.createElement("button"));
     learnMore.className = "learn-more";
     learnMore.innerHTML = 
-      '<a href="www.urbandesignx.com"><p style="color: #2399d5!important; padding-bottom: 12px; padding-top: -15px;">Learn more</p></a>';
+      '<a href="'+ prop[config.popupLink] +'"><p style="color: #2399d5!important; padding-bottom: 12px; padding-top: -15px;">Learn more</p></a>';
     
 
     link.addEventListener("click", function () {

048b5f4911c8c6bcaa4bf6903a9e6a9200927367
callumaoconnor <93537697+callumaoconnor@users.noreply.github.com>
2022-03-07 15:38:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -430,15 +430,91 @@ geocoder.on("result", function (ev) {
 map.on("load", function () {
   map.addControl(geocoder, "top-right");
   
+  
+  
+  
   map.addSource('projectPins', {
 type: 'geojson',
 // Point to GeoJSON data. This example visualizes all M1.0+ projectPins
 // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-data: geojsonData,
+data: 'geojsonData',
 cluster: true,
 clusterMaxZoom: 14, // Max zoom to cluster points on
 clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
 });
+map.addLayer({
+id: 'clusters',
+type: 'circle',
+source: 'projectPins',
+filter: ['has', 'point_count'],
+paint: {
+// Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+// with three steps to implement three types of circles:
+//   * Blue, 20px circles when point count is less than 100
+//   * Yellow, 30px circles when point count is between 100 and 750
+//   * Pink, 40px circles when point count is greater than or equal to 750
+'circle-color': [
+'step',
+['get', 'point_count'],
+'#51bbd6',
+100,
+'#f1f075',
+750,
+'#f28cb1'
+],
+'circle-radius': [
+'step',
+['get', 'point_count'],
+20,
+100,
+30,
+750,
+40
+]
+}
+});
+map.addLayer({
+id: 'cluster-count',
+type: 'symbol',
+source: 'projectPins',
+filter: ['has', 'point_count'],
+layout: {
+'text-field': '{point_count_abbreviated}',
+'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+'text-size': 12
+}
+});
+map.addLayer({
+id: 'unclustered-point',
+type: 'circle',
+source: 'projectPins',
+filter: ['!', ['has', 'point_count']],
+paint: {
+'circle-color': '#11b4da',
+'circle-radius': 4,
+'circle-stroke-width': 1,
+'circle-stroke-color': '#fff'
+}
+});
+// inspect a cluster on click
+map.on('click', 'clusters', (e) => {
+const features = map.queryRenderedFeatures(e.point, {
+layers: ['clusters']
+});
+const clusterId = features[0].properties.cluster_id;
+map.getSource('projectPins').getClusterExpansionZoom(
+clusterId,
+(err, zoom) => {
+if (err) return;
+map.easeTo({
+center: features[0].geometry.coordinates,
+zoom: zoom
+});
+}
+);
+
+  
+  
 
   // csv2geojson - following the Sheet Mapper tutorial https://www.mapbox.com/impact-tools/sheet-mapper
   console.log("loaded");
@@ -473,100 +549,13 @@ clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 
         });
 
 
-        // Add the the layer to the map
-        map.addLayer({
-          id: 'clusters',
-          type: 'circle',
-          source: 'projectPins',
-          filter: ['has', 'point_count'],
-          paint: {
-          // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
-          // with three steps to implement three types of circles:
-          //   * Blue, 20px circles when point count is less than 100
-          //   * Yellow, 30px circles when point count is between 100 and 750
-          //   * Pink, 40px circles when point count is greater than or equal to 750
-          'circle-color': [
-          'step',
-          ['get', 'point_count'],
-          '#51bbd6',
-          100,
-          '#f1f075',
-          750,
-          '#f28cb1'
-          ],
-          'circle-radius': [
-          'step',
-          ['get', 'point_count'],
-          20,
-          100,
-          30,
-          750,
-          40
-          ]
-          }
-          });
-          map.addLayer({
-          id: 'cluster-count',
-          type: 'symbol',
-          source: 'projectPins',
-          filter: ['has', 'point_count'],
-          layout: {
-          'text-field': '{point_count_abbreviated}',
-          'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-          'text-size': 12
-          }
-         });
-          map.addLayer({
-          id: 'unclustered-point',
-          type: 'circle',
-          source: 'projectPins',
-          filter: ['!', ['has', 'point_count']],
-          paint: {
-          'circle-color': '#11b4da',
-          'circle-radius': 4,
-          'circle-stroke-width': 1,
-          'circle-stroke-color': '#fff'
-         }
-        });
-          // inspect a cluster on click
-          map.on('click', 'clusters', (e) => {
-          const features = map.queryRenderedFeatures(e.point, {
-          layers: ['clusters']
-          });
-          const clusterId = features[0].properties.cluster_id;
-          map.getSource('projectPins').getClusterExpansionZoom(
-          clusterId,
-          (err, zoom) => {
-          if (err) return;
-          map.easeTo({
-          center: features[0].geometry.coordinates,
-          zoom: zoom
-          });
-          }
-         );
-        });
 
-
-    map.on("click", "locationData", function (e) {
-      const features = map.queryRenderedFeatures(e.point, {
-        layers: ["locationData"],
-      });
-      const clickedPoint = features[0].geometry.coordinates;
-      flyToLocation(clickedPoint);
-      sortByDistance(clickedPoint);
-      createPopup(features[0]);
-    });
-
-    map.on("mouseenter", "locationData", function () {
-      map.getCanvas().style.cursor = "pointer";
-    });
-
-    map.on("mouseleave", "locationData", function () {
-      map.getCanvas().style.cursor = "";
-    });
-    buildLocationList(geojsonData);
-  }
-});
+        
+        
+        
+        
+        
+        
 
 // Modal - popup for filtering results
 const filterResults = document.getElementById("filterResults");

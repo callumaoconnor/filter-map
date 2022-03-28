274f0ffeed303a98bdac1e2a6ed8476f70615d9e
callumaoconnor <93537697+callumaoconnor@users.noreply.github.com>
2022-03-28 10:27:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function buildLocationList(locationData) {
     
     
     /* Assign the `item` class to each listing for styling. */
-    if (prop[config.formatInfo] != "X") {
+    if (prop[config.formatInfo] != "x") {
     listing.className = "item"} else {
     listing.className = "none";
     }                           
